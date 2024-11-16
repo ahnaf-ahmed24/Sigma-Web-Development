54bdcc441f794e7c0ef7df3d6ac7fc1c70da4a78
Ahnaf Ahmed <166901576+ahnaf-ahmed24@users.noreply.github.com>
2024-11-16 12:23:44 +0600

    Video 54

---
+++ VIDEO 54/new.js
@@ -0,0 +1,4 @@
+console.log("Hello World")
+console.log("Code is Runnning....")
+console.log("Code is also Runnning....")
+console.log("Code is looking like a Wow....")
---
+++ VIDEO 54/script.js
@@ -0,0 +1,16 @@
+alert("Hello World");
+
+console.log("Code is Runnning....")
+console.log("Code is also Runnning....")
+console.log("Code is looking like a Wow....")
+
+var a = prompt("Enter Your Number : ")
+var isTrue = confirm("Are You sure you want top leave this page and blast your computer")
+
+if(isTrue){
+    console.log("computer is Blasting")
+}
+else{
+    console.log("computer is not Blasting")
+}
+console.log("Your number is " + a )
