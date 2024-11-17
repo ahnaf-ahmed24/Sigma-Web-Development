60d726dda7dc7e3c070b737741c98077ef9d24e3
Ahnaf Ahmed <166901576+ahnaf-ahmed24@users.noreply.github.com>
2024-11-17 19:56:21 +0600

    Video 55

---
+++ VIDEO 55/script.js
@@ -0,0 +1,51 @@
+console.log("Hey this is tutorial 55")
+
+// var a = 5;
+// a = a + 1;
+// var b = 5;
+// var c = "Ahnaf"
+
+let a = 5;
+a = a + 1;
+let b = 5;
+let c = "Ahnaf"
+
+// var 99b = "Ahmed"  // Not Allowed
+
+// console.log(a + b + 8)
+// console.log(typeof a, typeof b, typeof c)
+
+{
+    let a = 66
+    console.log(a)
+}
+
+console.log(a)
+
+// const a1 = 6;
+// a1 = a1 + 1;
+// Not Allowed because a1 is constant
+
+
+let x = "Ahnaf Ahmed Antor"
+let y = 22; 
+let z = 3.55;
+const p = true;
+let q = undefined;
+let r = null;
+
+console.log(x, y, z, p, q, r)
+console.log(typeof x, typeof y, typeof z, typeof p, typeof q, typeof r)
+
+
+let o = {
+    name : "Ahnaf",
+    "job code" : 5600,
+    "is_handsome" : true
+}
+
+console.log(o)
+o.salary = "100 Crores";
+console.log(o)
+o.salary = "500 Crores";
+console.log(o)
