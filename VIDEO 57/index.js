aed261414ed20572bee8c0bae11efe543cfed5a2
Ahnaf Ahmed <166901576+ahnaf-ahmed24@users.noreply.github.com>
2024-11-22 20:52:51 +0600

    Video 57

---
+++ VIDEO 57/index.js
@@ -0,0 +1,42 @@
+console.log("I am a tutorialon Loops")
+
+// let a = 1;
+// console.log(a)
+// console.log(a + 1)
+// console.log(a + 2)
+
+// for (let i = 0; i < 100; i++) {
+//     console.log(a + i);
+// }
+
+// let obj = {
+//     name: "Ahnaf Ahmed Antor",
+//     job: "Programmer",
+//     company: "CordwtihAhnaf AI"
+// }
+
+// for (const key in obj) {
+//     const element = obj[key];
+//     console.log(key, element)
+// }
+
+// for (const key in obj) {
+//     console.log(key)
+// }
+
+// for (const c of "Ahnaf Ahhmed Antor") {
+//     console.log(c)
+// }
+
+// let i = 0;
+// while (i<6) {
+//     console.log(i)
+//     i++
+// }
+
+
+let i = 10;
+do {
+    console.log(i)
+    i++;
+} while (i<6);
