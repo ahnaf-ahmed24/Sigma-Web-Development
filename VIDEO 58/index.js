75cd9f5838b7889305cc00950b9e1da3d965ae16
Ahnaf Ahmed <166901576+ahnaf-ahmed24@users.noreply.github.com>
2024-11-23 11:26:57 +0600

    Video 58

---
+++ VIDEO 58/index.js
@@ -0,0 +1,41 @@
+// function nice(name) {
+//     console.log("Hey " + name + " you are nice!")
+//     console.log("Hey " + name + " you are good!")
+//     console.log("Hey " + name + " your tshirt is nice!")
+//     console.log("Hey " + name + " your course is good tooo!")
+// }
+
+// nice("Ahmed")
+// nice("Konok")
+
+
+
+// console.log("Hey Ahnaf you are nice!")
+// console.log("Hey Ahnaf you are good!")
+// console.log("Hey Ahnaf your tshirt is nice!")
+// console.log("Hey Ahnaf your course is good tooo!")
+
+function sum(a, b, c = 3) {
+    // console.log(a + b)
+    console.log(a, b, c)
+    return a + b + c
+}
+
+result1 = sum(3, 5)
+result2 = sum(7, 5)
+result3 = sum(3, 13, 1)
+
+
+console.log("The sum of these numbers is:", result1)
+console.log("The sum of these numbers is:", result2)
+console.log("The sum of these numbers is:", result3)
+
+
+const func1 = (x)=>{
+    console.log("I am an arrow function", x)
+}
+
+func1(34)
+func1(44)
+func1(55)
+func1(67)
