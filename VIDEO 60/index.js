3cf0ba603ef0fba7fb6667ff413fbfaa55deabba
Ahnaf Ahmed <166901576+ahnaf-ahmed24@users.noreply.github.com>
2024-11-25 11:45:14 +0600

    Video 60

---
+++ VIDEO 60/index.js
@@ -0,0 +1,30 @@
+console.log("This is string tutorial")
+
+let a = "Ahnaf"
+console.log(a)
+console.log(a[0])
+console.log(a[1])
+console.log(a[2])
+console.log(a[3])
+console.log(a[4])
+// console.log(a[5])
+
+console.log(a.length)
+
+let real_name = "Ahmed"
+let friend = "Konok"
+
+// console.log("His name is " + real_name + " His friends is " + friend)
+
+console.log(`His name is ${real_name} and His friends is ${friend}`)
+
+
+let b = "Ahnaf Ahmed "
+console.log(b.toUpperCase()) 
+console.log(b.toLowerCase()) 
+console.log(b.length) 
+console.log(b.slice(1,4)) 
+console.log(b.slice(1))
+console.log(b.replace("Ahmed", "Antor")) 
+console.log(b.concat(a, " Antor"))
+
