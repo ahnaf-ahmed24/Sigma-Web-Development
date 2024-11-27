bbf46378cff1aa9e863acd3a75d2b99a312223cf
Ahnaf Ahmed <166901576+ahnaf-ahmed24@users.noreply.github.com>
2024-11-27 20:50:26 +0600

    Video 63

---
+++ VIDEO 63/index.js
@@ -0,0 +1,21 @@
+let arr = [1, 2, 4, 5, 7, 9, 11, 13]
+
+
+arr[0] = 5666;
+// console.log(arr)
+// console.log(arr.length)
+// console.log(arr[0])
+// console.log(arr[2])
+// console.log(arr[4])
+console.log(arr.toString())
+console.log(arr.join( " and "))
+console.log(arr.pop())
+console.log(arr)
+console.log(arr.push("Ahnaf"))
+console.log(arr)
+console.log(arr.shift())
+console.log(arr)
+console.log(arr.unshift("Ahmed"))
+console.log(arr)
+console.log(arr.length)
+console.log(arr[8])
