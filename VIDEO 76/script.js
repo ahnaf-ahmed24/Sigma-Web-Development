f208a1852b4185a910d02a49c67cd28a38b55a4f
Ahnaf Ahmed <166901576+ahnaf-ahmed24@users.noreply.github.com>
2024-12-15 21:08:28 +0600

    Add files via upload

--- VIDEO 76/script.js
+++ VIDEO 76/script.js
@@ -1,11 +1,59 @@
-function getData(){
+// async function getData() {
+//     // Simulate getting Data from a Server
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             resolve(455)
+//         }, 3500);
+//     })
+// }
+
+// async function getData() {
+//     // Simulate getting Data from a Server
+//     let x = await fetch('https://jsonplaceholder.typicode.com/todos/1')
+//     let data = await x.json()
+//       return data
+// }
+
+async function getData() {
+    // Simulate getting data from a server
+    let x = await fetch('https://jsonplaceholder.typicode.com/posts', {
+                method: 'POST',
+                body: JSON.stringify({
+                    title: 'foo',
+                    body: 'bar',
+                    userId: 1,
+                }),
+                headers: {
+                    'Content-type': 'application/json; charset=UTF-8',
+                },
+            })
+    let data = await x.json() 
+    return data
+}
+
+async function main() {
+    
+    console.log("Loding modules")
+    
+    console.log("Do something else")
+    
+    console.log("Lode Data")
+    
+    let data = await getData()
+    console.log(data)
+
+console.log("Process data")
+
+console.log("task 2")
 
 }
 
-console.log("Loding modules")
+main()
 
-console.log("Do something else")
+// data.then((v) => {
+//     console.log("data")
 
-console.log("Lode Data")
+//     console.log("Process data")
 
-let data = getData
+//     console.log("task 2")
+// })
